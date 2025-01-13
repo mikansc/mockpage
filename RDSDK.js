f23a9fb863e00c567496c0640690977264bd6f87
Michael Nascimento <michael.nsc@outlook.com>
2025-01-13 16:58:46 -0300

    Create RDSDK.js

---
+++ RDSDK.js
@@ -0,0 +1,9 @@
+/**
+  * Resultados Digitais - 1/13/2025, 7:48:25 PM
+  * webpush-client - Project that controls the webpush notifications and permissions
+  * @version v1.0.0
+  * @link undefined
+  * @license MIT
+  * @environment staging
+  */
+var WebpushClient;(()=>{var t="https://d2umgtt5tsqx05.cloudfront.net".concat("/js/webpush-client/service-worker.min.js");importScripts(t),WebpushClient={}})();
